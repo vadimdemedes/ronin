595fcda8c260fe6f3209f0a4c03a86b92af1eccb
vdemedes <vdemedes@gmail.com>
2014-12-03 11:57:44 +0100

    using yeoman for generating new programs

--- bin/skeleton/program/$name.js
+++
@@ -1,5 +0,0 @@
-var ronin = require('ronin');
-
-var program = ronin(__dirname);
-
-program.run();
--- bin/skeleton/program/commands/hello.js
+++
@@ -1,9 +0,0 @@
-var Command = require('ronin').Command;
-
-var HelloCommand = module.exports = Command.extend({
-	desc: 'Command that says hello',
-	
-	run: function () {
-		console.log('Hello World!');
-	}
-});
