466f0c4792a82cbb942b673f252a7d3aeffbca99
vdemedes <vdemedes@gmail.com>
2015-08-26 11:30:35 +0300

    remove transpiling via babel

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Program = require('./build/program');
-var Command = require('./build/command');
+var Program = require('./lib/program');
+var Command = require('./lib/command');
 
 var exports = module.exports = function createProgram (options) {
 	return new Program(options);
